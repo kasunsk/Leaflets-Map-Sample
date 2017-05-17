c4bd2f5bcde40ad90b1bce57ae99d2b17c8bb814
Kasun Kariyawasam <kasunsk@gmail.com>
2017-05-17 16:56:13 +0000

    First commit

---
+++ src/main/java/com/kasun/mapsample/MapSampleApplication.java
@@ -0,0 +1,12 @@
+package com.kasun.mapsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapSampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kasun/mapsample/MapSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kasun.mapsample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class MapSampleApplicationTests {
+
+//	@Test
+//	public void contextLoads() {
+//	}
+
+}
