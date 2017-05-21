7b95b5ff17ad5e02e5bf1158fa0f3f3d2082dfcf
Kasun Kariyawasam <kasunsk@gmail.com>
2017-05-21 12:39:20 +0000

    added leaflet search

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -2,7 +2,8 @@
  * Created by kasun on 5/17/17.
  */
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+var leaf_map = L.map('mapid').setView([7.872453, 80.771496], 7);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
@@ -10,4 +11,40 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
     'Imagery © <a href="http://mapbox.com">Mapbox</a>',
     id: 'mapbox.streets'
-}).addTo(mymap);
+}).addTo(leaf_map);
+
+L.marker([7.884677, 80.534522]).addTo(leaf_map);
+
+L.marker([7.884677, 80.534522]).addTo(leaf_map).bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+
+// L.circle([7.90, 80.01], {
+//     color: 'red',
+//     fillColor: '#f03',
+//     fillOpacity: 0.5,
+//     radius: 1000
+// }).addTo(leaf_map);
+
+L.polygon([
+    [7.80, 80.6],
+    [7.89, 80.5],
+    [7.85, 80.4]
+]).addTo(leaf_map);
+
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+
+leaf_map.addControl(new L.Control.Search({
+    url: 'https://nominatim.openstreetmap.org/search?format=json&q={s}',
+    jsonpParam: 'json_callback',
+    propertyName: 'display_name',
+    propertyLoc: ['lat', 'lon'],
+    circleLocation: false,
+    markerLocation: false,
+    autoType: false,
+    autoCollapse: true,
+    minLength: 2,
+    zoom: 12
+}));
+
+leaf_map.on('click', onMapClick);
